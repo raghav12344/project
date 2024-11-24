945b38ce4c48d8a196eb8155604c1181ddb58f96
raghav12344 <graghav1233@gmail.com>
2024-11-24 18:25:23 +0530

    my projecct

---
+++ database.sql
@@ -0,0 +1,19 @@
+use defaultdb;
+-- users table 
+create table users(emailid varchar(500) primary key,pwd varchar(30),utype varchar(20),dos date,status int);
+select * from users;
+delete from users;
+drop table users;
+-- players table 
+create table players(emailid varchar(500) primary key, fullname varchar(500),gender varchar(50),mobile varchar(30),address varchar(1000),city varchar(40),state varchar(40),zip varchar(20),proof varchar(30),proofpath varchar(5000),games varchar(500),prev_achv varchar(500),insta varchar(100));
+select * from players;
+-- organisers table
+select * from organizers;
+delete  from organizers;
+drop table organizers;
+create table organizers(email varchar(500) primary key,organization varchar(500),contact varchar(30),address varchar(1000),city varchar(40),state varchar(40),zip varchar(20),proof varchar(30),proofpath varchar(5000),sports varchar(500),hosted varchar(1000),website varchar(500),intagram varchar(100));
+create table tournaments(tid int primary key auto_increment,email varchar(500),mobile varchar(20),game varchar(100),title varchar(100),fee varchar(10),dot date,city varchar(40),state varchar(50),zip varchar(30),location varchar(500),prizes varchar(1000),posterpic varchar(5000),info varchar(5000));
+drop table tournaments;
+select * from tournaments;
+drop table tournaments;
+select DISTINCT game from tournaments;
---
+++ server.js
@@ -0,0 +1,233 @@
+var express=require("express");
+var app=express();
+var mysql=require("mysql2");
+let config="mysql://avnadmin:AVNS_AdrypzLPwFLOySPl7dA@mysql-1e633ea7-raghav1233-61dd.k.aivencloud.com:28277/defaultdb?";
+let db=mysql.createConnection(config);
+let fileuploader=require("express-fileupload");
+var cloudinary=require("cloudinary").v2;
+cloudinary.config({
+    cloud_name: 'drsm7bvgf', 
+    api_key: '441925695211367', 
+    api_secret: '-jhKZ_AH-55LErkx_jPF1wT89ec'
+});
+app.use(fileuploader());
+app.use(express.urlencoded(true));
+db.connect(function(err){
+    if(err==null)
+        console.log("connected to database");
+    else
+        console.log(err.message);
+})
+app.use(express.static("public"));
+app.listen(1000,function(req,resp){
+    console.log("server started at 1000");
+})
+app.get("/",function(req,resp){
+    let path=__dirname+"/public/index.html";
+    resp.sendFile(path);
+})
+app.get("/signup",function(req,resp){
+    let email=req.query.txtEmail;
+    let pwd=req.query.txtPwd;
+    let utype=req.query.utype;
+
+    db.query("insert into users(emailid,pwd,utype,status,dos) values(?,?,?,?,current_date())",[email,pwd,utype,1],function(err){
+        if(err==null)
+           resp.send("Signed up Successfully.Please Login To Continue");
+        else
+            resp.send(err.message)
+    })
+
+})
+app.get("/login",function(req,resp){
+    let email=req.query.txtEmaillog;
+    let pwd=req.query.txtPwdlog;
+    db.query("select * from users where emailid=? and pwd=?",[email,pwd],function(err,jsonArray){
+        // resp.send(jsonArray)
+        console.log(jsonArray);
+        if(jsonArray.length==1)
+        {
+            resp.send(jsonArray[0]["utype"]);
+            console.log(jsonArray[0]["status"]);
+        }
+        else
+            resp.send("incorrect credentials");
+    })
+    
+})
+app.get("/profileorganizer",function(req,resp){
+    let path=__dirname+"/public/profileorganizer.html"
+    resp.sendFile(path);
+})
+app.post("/savedetails",async function(req,resp){
+    let email=req.body.txtemail;
+    let mobile=req.body.txtmobile;
+    let org=req.body.txtname;
+    let address=req.body.txtadd;
+    let city=req.body.txtcity;
+    let zip=req.body.txtzip;
+    let state=req.body.txtstate;
+    let sports=req.body.sports;
+    let proof=req.body.txtproof;
+    let i=0;
+    // console.log(sports);
+    let strsports="";
+    for(i=0;i<sports.length;i+=1)
+    {
+        strsports=strsports+","+sports[i];
+    }
+    let previous=req.body.txthosted;
+    let web=req.body.txtweb;
+    let insta=req.body.txtinsta;
+    let filename="";
+    console.log(req.body);
+    filename=req.files.proofpic.name;
+    let path=__dirname+"/public/uploads/"+filename;
+    console.log(path);
+    req.files.proofpic.mv(path);
+
+    await cloudinary.uploader.upload(path).then(function(result){
+        filename=result.url;
+        console.log(filename);
+    });
+    db.query("insert into organizers values(?,?,?,?,?,?,?,?,?,?,?,?,?)",[email,org,mobile,address,city,state,zip,proof,filename,strsports,previous,web,insta],function(err){
+        if(err==null)
+            resp.send("saved successfully");
+        else
+            resp.send(err.message);
+    })
+})
+app.get("/load-publish-tournaments",function(req,resp){
+    let path=__dirname+"/public/publish-tournaments.html";
+    resp.sendFile(path);
+})
+app.post("/post-tournament",async function(req,resp){
+    let email=req.body.txtemail;
+    let mobile=req.body.txtmobile;
+    let game=req.body.txtgame;
+    let title=req.body.txttitle;
+    let fee=req.body.txtfee;
+    let city=req.body.txtcity;
+    let state=req.body.txtstate;
+    let zip=req.body.txtzip;
+    let add=req.body.txtaddress;
+    let prizes=req.body.txtprizes;
+    let dot=req.body.date_dot;
+    let filename="";
+    filename=req.files.picposter.name;
+    // console.log(filename);
+    let path=__dirname+"/public/uploads/"+filename;
+    req.files.picposter.mv(path);
+    await cloudinary.uploader.upload(path).then(function(result){
+        filename=result.url;
+    });
+    let info=req.body.txtinfo;
+    db.query("insert into tournaments values(?,?,?,?,?,?,?,?,?,?,?,?,?,?)",[null,email,mobile,game,title,fee,dot,city,state,zip,add,prizes,filename,info],function(err){
+        if(err==null)
+            resp.send("posted successfully")
+        else
+            resp.send(err.message);
+    })
+
+})
+app.get("/find-tournament",function(req,resp){
+    let path=__dirname+"/public/tournaments-finder.html";
+    resp.sendFile(path);
+})
+app.get("/fetch-cities", function(req,resp){
+     db.query("select DISTINCT city from tournaments",function(err,jsonArray){
+        if(err==null)
+        {
+            resp.send(jsonArray);
+            // console.log(jsonArray);
+        }
+        else 
+            resp.send(err.message);
+    })
+
+})
+app.get("/fetch-sports", function(req,resp){
+    db.query("select DISTINCT game from tournaments",function(err,jsonArray){
+       if(err==null)
+       {
+           resp.send(jsonArray);
+           // console.log(jsonArray);
+       }
+       else 
+           resp.send(err.message);
+   })
+
+})
+app.get("/search-tournaments",function(req,resp){
+    let city=req.query.city;
+    let game=req.query.game;
+
+    db.query("select * from tournaments where  city=? and game=?",[city,game],function(err,jsonArray){
+        if(err==null)
+        {
+            // console.log(jsonArray);
+            resp.send(jsonArray);
+        }
+        else
+        resp.send(err.message);
+    })
+})
+app.get("/profileplayer",function(req,resp){
+    let path=__dirname+"/public/profileplayer.html";
+    resp.sendFile(path);
+})
+app.post("/savedetailsplayer",async function(req,resp){
+    let email=req.body.txtemail;
+    let mobile=req.body.txtmobile;
+    let gender=req.body.txtgender;
+    let fullname=req.body.txtfullname;
+    let address=req.body.txtadd;
+    let city=req.body.txtcity;
+    let zip=req.body.txtzip;
+    let state=req.body.txtstate;
+    let games=req.body.txtgames;
+    let previous=req.body.txtachv;
+    let insta=req.body.txtinsta;
+    let proof=req.body.txtproof;
+    // console.log(proof);
+    let filename="";
+    console.log(req.body);
+    filename=req.files.proofpic.name;
+    let path=__dirname+"/public/uploads/"+filename;
+    // console.log(path);
+    req.files.proofpic.mv(path);
+    await cloudinary.uploader.upload(path).then(function(result){
+        filename=result.url;
+        console.log(filename);
+    });
+    db.query("insert into players values(?,?,?,?,?,?,?,?,?,?,?,?,?)",[email,fullname,gender,mobile,address,city,state,zip,proof,filename,games,previous,insta],function(err){
+        if(err==null)
+            resp.send("saved successfully");
+        else
+            resp.send(err.message);
+    })
+})
+app.get("/searchplayers",function(req,resp){
+    let email=req.query.txtemail;
+
+    db.query("select * from players where emailid=?",[email],function(err){
+        if(err==null)
+            resp.send("user found");
+        else
+            resp.send(err.message);
+    })
+})
+app.get("/change-settings",function(req,resp){
+    let email=req.query.txtemail;
+    let curr=req.query.txtcurr;
+    let newpass=req.query.txtnew;
+
+    db.query("update users set pwd=? where emailid=? and pwd=?",[newpass,email,curr],function(err,result){
+        if (err!=null)
+            resp.send(err.message);
+        else if(result.affectedRows==1)
+            resp.send("password successfully changed");
+        else
+            resp.send("invalid password");
+    })
+})
